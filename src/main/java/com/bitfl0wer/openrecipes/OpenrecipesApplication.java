f02af82a8a48f412af714b7964fa91ce50aed7bc
bitfl0wer <florian@pro-weber.com>
2023-01-23 21:58:08 +0100

    Simple controller to check my setup

--- src/main/java/com/bitfl0wer/openrecipes/OpenrecipesApplication.java
+++ src/main/java/com/bitfl0wer/openrecipes/OpenrecipesApplication.java
@@ -2,6 +2,11 @@ package com.bitfl0wer.openrecipes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class OpenrecipesApplication {
