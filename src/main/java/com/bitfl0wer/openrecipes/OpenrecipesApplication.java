90c27185a456bf60c014d04ca35417364afbf2f7
bitfl0wer <florian@pro-weber.com>
2023-01-23 22:01:21 +0100

    Remove unnecessary imports

--- src/main/java/com/bitfl0wer/openrecipes/OpenrecipesApplication.java
+++ src/main/java/com/bitfl0wer/openrecipes/OpenrecipesApplication.java
@@ -2,11 +2,6 @@ package com.bitfl0wer.openrecipes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class OpenrecipesApplication {
